f06b4b5bc7afaa35e1492f1535f638939f76cc84
jasminejohnpj <jasminejohnpj@gmail.com>
2025-01-17 11:43:48 +0530

    index.js updated

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express= require('express');
 const cors = require('cors');
 const app= express();
 const http= require('http');
+app.use(express.json({ limit: "10mb" }));
 
 app.use(cors());
-app.use(express.json());
 
  app.use('/api/v1',require('./router/routing'));
 
