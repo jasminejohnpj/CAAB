00546f2f6163cbff5c7a4e101196d2d40c90e03d
jasminejohnpj <jasminejohnpj@gmail.com>
2025-04-02 10:16:16 +0530

    law model modified

--- model/law.js
+++ model/law.js
@@ -4,13 +4,13 @@ const {DataTypes,Sequelize}= require('sequelize');
 const db = require('./db');
 
 const laws = db.define('law',{
-    department_name:{type:DataTypes.STRING},
-    law:{type:DataTypes.STRING },
-    act_rule:{type:DataTypes.STRING },
-    section:{type:DataTypes.STRING},
-    penalty_amount:{type:DataTypes.INTEGER},
-    due_date:{type:DataTypes.STRING},
-    alert_date:{type:DataTypes.STRING},
+    department_name:{ type:DataTypes.STRING },
+    law:{ type:DataTypes.STRING },
+    act_rule:{ type:DataTypes.STRING },
+    section:{ type:DataTypes.STRING },
+    penalty_amount:{ type:DataTypes.INTEGER },
+    due_date:{ type:DataTypes.STRING },
+    alert_date:{ type:DataTypes.STRING },
     
 
 },{timestamps:false});
